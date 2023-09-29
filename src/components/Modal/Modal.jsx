ee50a003343693efd9b2c14782da8689ddc97994
KarinaPavliuk <kpavliuk8@gmail.com>
2023-09-29 14:06:54 +0300

    update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,82 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import css from './Modal.module.css';
 
-export class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyEsc);
-  }
+export const Modal = ({ modalUrl, closeModal }) => {
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyEsc);
+  }, []);
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyEsc);
-  }
+  useEffect(() => {
+    return document.removeEventListener('keydown', handleKeyEsc);
+  }, []);
 
-  handleKeyEsc = event => {
+  const handleKeyEsc = event => {
     if (event.code === 'Escape') {
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  handleOverlayClick = event => {
+  const handleOverlayClick = event => {
     if (event.target === event.currentTarget) {
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  render() {
-    return (
-      <>
-        <div className={css.overlay} onClick={this.handleOverlayClick}>
-          <div className={css.modal}>
-            <img src={this.props.modalUrl} alt="" />
-          </div>
+  return (
+    <>
+      <div className={css.overlay} onClick={handleOverlayClick}>
+        <div className={css.modal}>
+          <img src={modalUrl} alt="" />
         </div>
-        {/* {this.props.images.map(({ id, largeImageURL, tags }) => (
-          <div className={css.overlay}>
-            <div className={css.modal}>
-              <img key={id} src={largeImageURL} alt={tags} />
+      </div>
+      {/* {this.props.images.map(({ id, largeImageURL, tags }) => (
+            <div className={css.overlay}>
+              <div className={css.modal}>
+                <img key={id} src={largeImageURL} alt={tags} />
+              </div>
             </div>
-          </div>
-        ))} */}
-      </>
-    );
-  }
-}
+          ))} */}
+    </>
+  );
+};
+
+// export class Modal extends Component {
+//   componentDidMount() {
+//     document.addEventListener('keydown', this.handleKeyEsc);
+//   }
+
+//   componentWillUnmount() {
+//     document.removeEventListener('keydown', this.handleKeyEsc);
+//   }
+
+//   handleKeyEsc = event => {
+//     if (event.code === 'Escape') {
+//       this.props.closeModal();
+//     }
+//   };
+
+//   handleOverlayClick = event => {
+//     if (event.target === event.currentTarget) {
+//       this.props.closeModal();
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <div className={css.overlay} onClick={this.handleOverlayClick}>
+//           <div className={css.modal}>
+//             <img src={this.props.modalUrl} alt="" />
+//           </div>
+//         </div>
+//         {/* {this.props.images.map(({ id, largeImageURL, tags }) => (
+//           <div className={css.overlay}>
+//             <div className={css.modal}>
+//               <img key={id} src={largeImageURL} alt={tags} />
+//             </div>
+//           </div>
+//         ))} */}
+//       </>
+//     );
+//   }
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,48 +1,41 @@
-import { Component } from 'react';
+import { useState } from 'react';
+
 import css from './Searchbar.module.css';
 
-export class Searchbar extends Component {
-  state = {
-    value: '',
-  };
+export const Searchbar = ({ submit }) => {
+  const [value, setValue] = useState('');
 
-  handleChange = ({ target: { value } }) => {
-    return this.setState({ value });
-  };
+  const handleChange = ({ target: { value } }) => setValue(value);
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.submit(this.state.value);
+    submit(value);
   };
 
-  clearInput = event => {
+  const clearInput = event => {
     event.target.value = '';
   };
 
-  render() {
-    const { handleSubmit, handleChange, clearInput } = this;
+  return (
+    <>
+      <header className={css.searchbar}>
+        <form className={css.form} onSubmit={handleSubmit}>
+          <button type="submit" className={css.button}>
+            <span className={css.buttonLabel}>Search</span>
+          </button>
 
-    return (
-      <>
-        <header className={css.searchbar}>
-          <form className={css.form} onSubmit={handleSubmit}>
-            <button type="submit" className={css.button}>
-              <span className={css.buttonLabel}>Search</span>
-            </button>
-
-            <input
-              className={css.input}
-              type="text"
-              autoComplete="off"
-              autoFocus
-              placeholder="Search images and photos"
-              onChange={handleChange}
-              onClick={clearInput}
-              value={this.state.value}
-            />
-          </form>
-        </header>
-      </>
-    );
-  }
-}
+          <input
+            className={css.input}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={handleChange}
+            onClick={clearInput}
+            value={value}
+          />
+        </form>
+      </header>
+    </>
+  );
+};
